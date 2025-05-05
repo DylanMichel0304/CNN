5808ce062fdc08812892e349bfb27d184fff645d
DylanMichel0304 <168269934+DylanMichel0304@users.noreply.github.com>
2025-05-05 22:47:00 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -21,8 +21,8 @@ class SimpleCNN(nn.Module):
         flattened_size = 128 * conv_output_size * conv_output_size 
         # Fully Connected Layers
         self.fc1 = nn.Linear(flattened_size, 512)
-        self.fc2 = nn.Linear(512, num_classes)  # Reinstated second FC layer
-        #self.fc3 = nn.Linear(256, num_classes) # Final layer (renamed from fc2)
+        self.fc2 = nn.Linear(512, num_classes)  
+        #self.fc3 = nn.Linear(256, num_classes) 
 
 
         # Dropouts after FCs
@@ -35,17 +35,17 @@ class SimpleCNN(nn.Module):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.pool3(F.relu(self.conv3(x)))
-        # x = self.pool4(F.relu(self.conv4(x))) # Commented out conv4/pool4
+        # x = self.pool4(F.relu(self.conv4(x))) 
 
-        x = self.dropout1(x) # Commented out dropout after convs
+        x = self.dropout1(x) 
 
         x = x.view(x.size(0), -1)  # Flatten
 
         x = F.relu(self.fc1(x))
-        #x = self.dropout1(x) # Apply dropout after first FC
+        #x = self.dropout1(x) 
 
-        #x = F.relu(self.fc2(x)) # Apply second FC layer
-        x = self.dropout2(x) # Apply second dropout
+        #x = F.relu(self.fc2(x))
+        x = self.dropout2(x) 
 
         # x = self.fc3(x) #
         x = self.fc2(x) 
