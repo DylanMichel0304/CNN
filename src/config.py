2c5bc104452ad511f570066eb867761ef2ada489
DylanMichel0304 <168269934+DylanMichel0304@users.noreply.github.com>
2025-05-05 22:31:55 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,20 +1,18 @@
 import torch
 
 # Data paths
-TRAIN_DIR = '/home/plouis/Documents/Cours/CNN/chest_xray/train'
-TEST_DIR = '/home/plouis/Documents/Cours/CNN/chest_xray/test'
+TRAIN_DIR = 
+TEST_DIR = #plus le meme 
 
-# Data parameters
-IMAGE_SIZE = 224 # Input image size (e.g., 224x224)
+#parameters
+IMAGE_SIZE = 224
 BATCH_SIZE = 128
 VALID_SPLIT = 0.2 # 20% for validation
-
-# Model parameters
 NUM_CLASSES = 3 # Normal, Virus, Bacteria
 
 # Training parameters
 LEARNING_RATE = 0.0001
-NUM_EPOCHS = 50 # Start with 10 epochs, can be adjusted
+NUM_EPOCHS = 50 #
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Model saving
