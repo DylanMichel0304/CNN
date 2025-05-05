63a64647fc352999b7df8d786005348812573367
DylanMichel0304 <168269934+DylanMichel0304@users.noreply.github.com>
2025-05-05 22:32:57 +0200

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 from . import config
 
 
-# Define transform for training data 
+# transform for training data 
 train_transform = transforms.Compose([
      transforms.Resize((config.IMAGE_SIZE, config.IMAGE_SIZE)),
      transforms.RandomHorizontalFlip(p=0.5), # 50% chance 
@@ -14,7 +14,7 @@ train_transform = transforms.Compose([
      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
  ])
 
-# Define transform for validation and testing 
+# transform for validation and testing 
 test_val_transform = transforms.Compose([
     transforms.Resize((config.IMAGE_SIZE, config.IMAGE_SIZE)),
     transforms.ToTensor(),
@@ -26,11 +26,11 @@ def get_dataloaders():
 
     train_dataset_aug = datasets.ImageFolder(config.TRAIN_DIR, transform=train_transform) 
     #train_dataset_no_aug = datasets.ImageFolder(config.TRAIN_DIR, transform=test_val_transform)
-    class_to_idx = train_dataset_no_aug.class_to_idx # Get class mapping from one instance
+    class_to_idx = train_dataset_no_aug.class_to_idx 
 
     test_dataset = datasets.ImageFolder(config.TEST_DIR, transform=test_val_transform)
 
-    num_train = len(train_dataset_aug) # Use length from the non-augmented instance
+    num_train = len(train_dataset_aug) 
     indices = list(range(num_train))
     split = int(np.floor(config.VALID_SPLIT * num_train))
 
@@ -41,7 +41,7 @@ def get_dataloaders():
     train_subset = Subset(train_dataset_aug, train_idx)
     valid_subset = Subset(train_dataset_aug, valid_idx) 
 
-    num_workers = 4 if config.DEVICE == "cuda" else 0 # Use workers for GPU, 0 for CPU often better
+    num_workers = 4 if config.DEVICE == "cuda" else 0 
     train_loader = DataLoader(train_subset, batch_size=config.BATCH_SIZE, shuffle=True, num_workers=num_workers, pin_memory=True if config.DEVICE == "cuda" else False)
     valid_loader = DataLoader(valid_subset, batch_size=config.BATCH_SIZE, shuffle=False, num_workers=num_workers, pin_memory=True if config.DEVICE == "cuda" else False)
     test_loader = DataLoader(test_dataset, batch_size=config.BATCH_SIZE, shuffle=False, num_workers=num_workers, pin_memory=True if config.DEVICE == "cuda" else False)
