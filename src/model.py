2a1b762f49f767b029d981317e786374378422a2
DylanMichel0304 <168269934+DylanMichel0304@users.noreply.github.com>
2025-05-05 22:40:00 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,19 +4,12 @@ import torch.nn.functional as F
 from . import config
 
 class SimpleCNN(nn.Module):
-    # def __init__(self, num_classes=config.NUM_CLASSES, dropout_prob=0.4): # Original dropout prob
     def __init__(self, num_classes=config.NUM_CLASSES, dropout_prob=0.5, dropout_prob2=0.3): # Added second dropout prob
         super(SimpleCNN, self).__init__()
-
-        # Convolutional Layer 1
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        # Convolutional Layer 2
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        # Convolutional Layer 3
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
